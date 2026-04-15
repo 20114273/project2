39cfed8dc29e086169bade1d3759304beb7d515c
sebblich <sebblich@gmail.com>
2026-04-15 15:59:53 +0200

    Kæmpe sortering

---
+++ Mega_MainSystem/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * cpu1_atmega.c
+ *
+ * Created: 15/04/2026 14.31.26
+ * Author : sebbl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
