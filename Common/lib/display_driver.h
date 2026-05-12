fc88db837b9b137c4fd541b88174eacd90e6b6dd
Boel <evboel@gmail.com>
2026-05-12 16:09:32 +0200

    update method hentSensorStatus

--- Common/lib/display_driver.h
+++ Common/lib/display_driver.h
@@ -1,12 +1,11 @@
 
-
 #ifndef DISPLAY_DRIVER_H_
 #define DISPLAY_DRIVER_H_
 
 void visBesked();
 void opstartBesked();
 void opdaterBesked(const char*, const char*);
-void hentSensorStatus();
+uint8_t hentSensorStatus();
 
 
 #endif /* DISPLAY_DRIVER_H_ */
